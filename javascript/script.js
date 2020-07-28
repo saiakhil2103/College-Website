eb05956e566219320d56089c22c9e9004654e754
saiakhil2103 <68911307+saiakhil2103@users.noreply.github.com>
2020-07-28 19:36:04 +0530

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+function home(){
+	window.location = "index.html";
+}
+
+function departments(){
+	window.location = "departments.html";
+}
+
+function nirf(){
+	window.location = "nirf.html";
+}
+		
